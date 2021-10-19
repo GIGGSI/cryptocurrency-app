5c136b96c5411d53c5320265fbf9dede5b8192a4
s.spasov <s.spasov@skillcircle.bg>
2021-10-20 01:25:31 +0300

    fixing mobile view of navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Menu, Typography, Avatar } from "antd";
 import { Link } from "react-router-dom";
 import icon from "../../images/cryptocurrency.png";
@@ -7,9 +7,29 @@ import {
   FundOutlined,
   BulbOutlined,
   MoneyCollectOutlined,
+  MenuOutlined,
 } from "@ant-design/icons/lib/icons";
 
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(false);
+  const [screenSize, setScreenSize] = useState(null);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize < 760) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(false);
+    }
+  }, [screenSize]);
+
   return (
     <div className="nav-container">
       <div className="logo-container">
@@ -17,21 +37,41 @@ const Navbar = () => {
         <Typography.Title level={2} className="logo">
           <Link to="/">Cryptoverse</Link>
         </Typography.Title>
+        <Button
+          className="menu-control-container"
+          onClick={() => setActiveMenu(!activeMenu)}
+        >
+          <MenuOutlined />
+        </Button>
       </div>
-      <Menu theme="dark">
-        <Menu.Item icon={<HomeOutlined />}>
-          <Link to="/">Home</Link>
-        </Menu.Item>
-        <Menu.Item icon={<FundOutlined />}>
-          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-        </Menu.Item>
-        <Menu.Item icon={<MoneyCollectOutlined />}>
-          <Link to="/exchanges">Exchenges</Link>
-        </Menu.Item>
-        <Menu.Item icon={<BulbOutlined />}>
-          <Link to="/news">News</Link>
-        </Menu.Item>
-      </Menu>
+      {activeMenu && (
+        <Menu theme="dark">
+          <Menu.Item
+            icon={<HomeOutlined />}
+            onClick={() => setActiveMenu(false)}
+          >
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item
+            icon={<FundOutlined />}
+            onClick={() => setActiveMenu(false)}
+          >
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item
+            icon={<MoneyCollectOutlined />}
+            onClick={() => setActiveMenu(false)}
+          >
+            <Link to="/exchanges">Exchenges</Link>
+          </Menu.Item>
+          <Menu.Item
+            icon={<BulbOutlined />}
+            onClick={() => setActiveMenu(false)}
+          >
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
+      )}
     </div>
   );
 };
